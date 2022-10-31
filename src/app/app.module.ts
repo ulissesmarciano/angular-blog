c002bb79a0c609f7764a4b039b035a593dbae4b1
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-10-31 17:33:53 -0300

    Criação do conteúdo dinâmico dos componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { SmallCardsComponent } from './components/small-cards/small-cards.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SmallCardsComponent } from './components/small-cards/small-cards.compon
     BigCardComponent,
     HomeComponent,
     MenuTitleComponent,
-    SmallCardsComponent
+    SmallCardsComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/small-cards/small-cards.component.ts
+++ src/app/components/small-cards/small-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-cards',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-cards.component.css']
 })
 export class SmallCardsComponent implements OnInit {
+
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardText=""
 
   constructor() { }
 
