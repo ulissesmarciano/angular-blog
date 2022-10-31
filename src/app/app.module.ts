32f0c49643cc90fc29ce158ab9ee6dcd363e4ef8
Ulisses Marciano Gonçalves <104742158+ulissesmarciano@users.noreply.github.com>
2022-10-31 16:39:17 -0300

    criação da página mocada.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallHorizontalCardComponent } from './components/small-horizontal-card/small-horizontal-card.component';
-import { SmallVerticalCardComponent } from './components/small-vertical-card/small-vertical-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     BigCardComponent,
-    SmallHorizontalCardComponent,
-    SmallVerticalCardComponent,
     HomeComponent,
-    MenuTitleComponent
+    MenuTitleComponent,
+    SmallCardsComponent
   ],
   imports: [
     BrowserModule,
